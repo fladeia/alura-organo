0236ed7fbecc5fb128b890a06a2950084e455553
fladeia <ladeiadev@gmail.com>
2022-11-13 09:12:08 -0300

    added design system to tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,39 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: {
+        white: "#FFFFFF",  
+        blue: "#6278F7",
+        grenn: "#95FFD4",
+        gray: "#AFAFAF",
+        black: "#212121"
+      },
+      secondary: {
+        programming: "#57C278",
+        frontend: "#82CFFA",
+        dataSciences: "#A6D157",
+        devops: "#E06B69",
+        uxDesign: "#DB6EBF",
+        mobile: "#FFBA05",
+        inovation: "#FF8A29"
+      },
+      tertiary: {
+        programming: "#D9F7E9",
+        frontend: "#E8F8FF",
+        dataSciences: "#F0F8E2",
+        devops: "#FDE7E8",
+        uxDesign: "#FAE9F5",
+        mobile: "#FFF5D9",
+        inovation: "#FFEEDF"
+      },
+    },
+    extend: { 
       fontFamily: {
-        "sans": "Poppins, sans-serif"
+        "sans": "Montserrat, sans-serif",
+        "serif": "Prata, sans-serif",
       }
     },
   },
